344f4e2b95520916dd7b9eec7ca2968a46cb469b
Ugendu Lilian <ugendulilian@gmail.com>
2022-11-03 13:29:39 +0100

    404 page and Error boundary components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {useState}from 'react';
 import './App.css';
 import {SiCoffeescript} from "react-icons/si"
-import coffeloverImage from './images/coffeelover.jpg';
 import useActions from './utils/useActions';
+import {IconContext} from 'react-icons';
+import ErrorFallback from './components/ErrorFallback';
+// import {ErrorBoundary} from 'react-error-boundary';
 
 
 
@@ -12,27 +14,28 @@ function App() {
 
   return (
     <>
-    <div className="logo">
-      <h1>CoffeeLover</h1>
-    </div>
-
-    
-    <div className="container">
-
-      Buy Coffee <SiCoffeescript/>: {state.count}
-      <br/>
-
-    <div className='buttons'>
-    <button onClick={() => dispatch({type: 'increment'})}>+</button>
-
-      <button onClick={() => dispatch({type: 'decrement'})}>-</button>
-
-      <button onClick = {() => dispatch({type: 'reset'})}>Reset</button> 
-
-      <button onClick = {() => dispatch({type: 'setValue', value: 10})}>Set Value</button>
-      </div>
-      
-      </div>
+      <div className='main'>
+        <div className="title">
+          <h1>CoffeeLover</h1>
+        </div>
+        <div className='counter'>
+          <h2>{state.count}</h2>
+        </div>
+        <div className='button-tabs'>
+          <div role={'button'} className='button-actions' onClick={() => dispatch({type: 'increment'})}>
+            <i className="fa fa-plus"></i>
+          </div>
+          <div role={'button'} className='button-actions' onClick={() => dispatch({type: 'decrement'})}>
+            <i className="fa fa-minus"></i>
+          </div>
+          <div role={'button'} className='button-actions' onClick = {() => dispatch({type: 'reset'})}>
+            <i className="fa fa-refresh"></i>
+          </div> 
+          <div className='counter-input'>
+            <input type="number" value={state.count} onChange={(e) => dispatch({type: 'setValue', value: Number(e.target.value)})}/>
+          </div>
+        </div>
+      </div>  
     </>
   )
 }
--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../App.css';
 import {SiCoffeescript} from "react-icons/si"
 import { Link } from "react-router-dom";
 
@@ -6,16 +7,19 @@ const NotFound = () => {
     return (
         <>
         <div className="container">
+            <div className="content">
             <h1>Ooopsieee!! Wrong Page</h1><br/>
-            <p>Try the link below</p>
+            <p>Try the link below</p><br />
+                
 
         <ul>
             <li>
-                <Link to= '/'>
+                <Link className= "coffee-link" to= '/'>
                 CoffeeLover <SiCoffeescript/>
                 </Link>
             </li>
         </ul>
+            </div>
         </div>
         
         
